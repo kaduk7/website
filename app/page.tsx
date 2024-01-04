30c3af3e3e630944e2c3acaf8d566a5402371bcc
Risky <riskytampati92@gmail.com>
2024-01-04 16:35:22 +0700

    coba

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div>9</div>
-=======
-    <div>2</div>
->>>>>>> b4e041cb2d7fd98b33a668de7f72d42f2f28a60e
+    <div>jj</div>
   )
 }
