8cfe1239afb19536c7be53f109820f4f7e8da6ee
vrotomo <vrotomomulya@gmail.com>
2024-01-09 15:15:53 +0700

    s

--- app/component/Servis.tsx
+++ app/component/Servis.tsx
@@ -95,10 +95,7 @@ const Servis = () => {
                             <h3>Call To Action</h3>
                             <p>
                                 {" "}
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                anim id est laborum.
+                                "Temukan inspirasi, buat perbedaan. Gabunglah dengan kami dalam aksi positif!"
                             </p>
                         </div>
                         <div className="col-lg-3 cta-btn-container text-center">
