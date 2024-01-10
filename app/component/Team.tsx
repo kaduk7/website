59bccb4c027e73ca6f5c3858f342314687455f25
Risky <riskytampati92@gmail.com>
2024-01-10 13:38:25 +0700

    s

--- app/component/Team.tsx
+++ app/component/Team.tsx
@@ -36,27 +36,6 @@ const Team = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay={300}>
-                            <div className="member d-flex align-items-start">
-                                <div className="pic">
-                                    <img
-                                        src="assets/img/team/team-2.jpg"
-                                        className="img-fluid"
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="member-info">
-                                    <h4>Wahyu Ningsih</h4>
-                                    <span>Administrasi dan Keuangan</span>
-                                    <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
-                                    <div className="social">
-                                        <a href="https://www.instagram.com/wningsih650/">
-                                            <i className="ri-instagram-fill" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div
                             className="col-lg-6 mt-4 mt-lg-0"
                             data-aos="zoom-in"
@@ -71,13 +50,13 @@ const Team = () => {
                                     />
                                 </div>
                                 <div className="member-info">
-                                    <h4>Dina Shilvia </h4>
-                                    <span>Sekretaris</span>
+                                    <h4>Wahyu Ningsih</h4>
+                                    <span>Administrasi dan Keuangan</span>
                                     <p>
                                         Aut maiores voluptates amet et quis praesentium qui senda para
                                     </p>
                                     <div className="social">
-                                        <a href="https://www.instagram.com/nsvhr/">
+                                        <a href="https://www.instagram.com/wningsih650/">
                                             <i className="ri-instagram-fill" />
                                         </a>
                                     </div>
@@ -212,6 +191,29 @@ const Team = () => {
                                     <p>
                                         Dolorum tempora officiis odit laborum officiis et et accusamus
                                     </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay={400}>
+                            <div className="member d-flex align-items-start">
+                                <div className="pic">
+                                    <img
+                                        src="assets/img/team/team-2.jpg"
+                                        className="img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="member-info">
+                                    <h4>Dina Shilvia</h4>
+                                    <span>Sekretaris</span>
+                                    <p>
+                                        Dolorum tempora officiis odit laborum officiis et et accusamus
+                                    </p>
+                                    <div className="social">
+                                        <a href="https://www.instagram.com/nsvhr/">
+                                            <i className="ri-instagram-fill" />
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
